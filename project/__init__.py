1a7e340e1184e8570db03bb48705d9f62294d772
Kirill Ivanov <55203533+kirill-ivanov-a@users.noreply.github.com>
2022-05-18 18:59:44 +0300

    Revert "Task 4. Dynamic connectivity (1/2)"

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,1 @@
-import project.bfs
-from project.bfs import *
-
-import project.shortest_path
-from project.shortest_path import *
-
-import project.triangle_counting
-from project.triangle_counting import *
+print("import sources directory")
